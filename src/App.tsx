5d292a62e7ee5480ff7917ecdfbce57deda86556
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-11 06:24:56 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import UserForm from "./components/UserForm";
 
 function App() {
   return (
     <div>
       <h1>managment system</h1>
+      <UserForm/>
     </div>
   );
 }
