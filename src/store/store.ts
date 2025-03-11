6941b409f96864f97c2f0699958d547569393e42
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-11 04:14:49 -0700

    add userReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import  userReducer from "./feature/userSlice";
 
 export const store = configureStore ({
-    reducer:{}
+    reducer:{
+        users: userReducer,
+    }
 })
 
 export type RootState = ReturnType<typeof store.getState>;
