97568c0a544e427cc6cb33662fc1d2cffe671697
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-12 09:25:42 -0700

    use varaible color

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -14,7 +14,7 @@ const Button:React.FC<ButtonProps> = ({children, onClick, type="button",classNam
     <button
         type={type}
         onClick={onClick}
-        className={`px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition ${className}`}
+        className={`px-4 py-2 bg-[var(--background-color-button)] text-[var(--text-button)] rounded-lg hover:bg-[var(--hover-color-button)] transition ${className}`}
     >
         {children}
     </button>
