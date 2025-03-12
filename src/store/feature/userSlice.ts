5ead8186e9cdd5d1efc633e3c5a393934ce59342
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-12 09:53:09 -0700

    add update and delete

--- src/store/feature/userSlice.ts
+++ src/store/feature/userSlice.ts
@@ -23,9 +23,27 @@ const userSlice = createSlice({
          state.push(newUser);
          localStorage.setItem("users", JSON.stringify(state));
           
+        },
+
+        updateUser: (state, action: PayloadAction<{ id: number; name: string; email: string }>) => {
+            const user = state.find ((user) => user.id === action.payload.id)
+            if(user){
+                user.name= action.payload.name;
+                user.email= action.payload.email;
+                localStorage.setItem("users", JSON.stringify(state))
+            }
+           
+          },
+          
+
+        deleteUser:(state, action:PayloadAction<number>)=>{
+            const updateUsers =  state.filter((user)=> user.id !== action.payload);
+            localStorage.setItem("users", JSON.stringify(updateUsers))
+            return updateUsers;
         }
+
     }
 })
 
-export const {addUser} = userSlice.actions;
+export const {addUser, deleteUser, updateUser} = userSlice.actions;
 export default userSlice.reducer;
