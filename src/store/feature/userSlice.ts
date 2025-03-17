adb451f1d86da6f103e13b459c23e7175eeee7d9
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-17 05:59:50 -0700

    update

--- src/store/feature/userSlice.ts
+++ src/store/feature/userSlice.ts
@@ -7,38 +7,37 @@ interface User{
   isActive: boolean;
 }
 
-const initialState : User[] = [];
+const initialState : User[] = []
 
-const userSlice = createSlice ({
+const userSlice = createSlice({
   name: "users",
   initialState,
   reducers:{
-    addUser:(state, action: PayloadAction<{name: string, email: string}>,)=>{
+    addUser:(state, action: PayloadAction<{name: string; email: string}>)=>{
       const newUser: User = {
-        id : Date.now(),
+        id: Date.now(),
         name: action.payload.name,
         email: action.payload.email,
-        isActive: true,
+        isActive:true,
       }
       state.push(newUser);
       localStorage.setItem("users", JSON.stringify(state))
     },
-    updateUser:(state, action: PayloadAction<{id: number; name: string; email: string}>,)=>{
-      const user = state.find((user) => user.id === action.payload.id);
+    updateUser:(state,action:PayloadAction<{id: number; name: string; email: string;}>)=>{
+      const user = state.find((user)=> user.id === action.payload.id);
       if(user){
         user.name= action.payload.name;
         user.email= action.payload.email;
         localStorage.setItem("users", JSON.stringify(state))
       }
-      },
-      deleteUser:(state, action: PayloadAction< number>)=>{
-        const updateUsers = state.filter((user) => user.id !== action.payload);
-        localStorage.setItem("users", JSON.stringify(updateUsers));
-        return updateUsers;
-      },
     },
+    deleteUser:(state, action: PayloadAction<number>)=>{
+      const updateUsers = state.filter((user)=> user.id!== action.payload);
+      localStorage.setItem("users", JSON.stringify(updateUsers));
+      return updateUsers
+    }
   }
-);
+})
 
-export const {addUser, updateUser,deleteUser} = userSlice.actions;
-export default userSlice.reducer;
+export const {addUser, updateUser, deleteUser} = userSlice.actions;
+export default userSlice.reducer
