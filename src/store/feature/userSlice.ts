2915d102f6d3bd0dbbd965f57f4cb78ddffc8de2
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-17 04:51:51 -0700

    update

---
+++ src/store/feature/userSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User{
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}
+
+const initialState : User[] = [];
+
+const userSlice = createSlice ({
+  name: "users",
+  initialState,
+  reducers:{
+    addUser:(state, action: PayloadAction<{name: string, email: string}>,)=>{
+      const newUser: User = {
+        id : Date.now(),
+        name: action.payload.name,
+        email: action.payload.email,
+        isActive: true,
+      }
+      state.push(newUser);
+      localStorage.setItem("users", JSON.stringify(state))
+    },
+    updateUser:(state, action: PayloadAction<{id: number; name: string; email: string}>,)=>{
+      const user = state.find((user) => user.id === action.payload.id);
+      if(user){
+        user.name= action.payload.name;
+        user.email= action.payload.email;
+        localStorage.setItem("users", JSON.stringify(state))
+      }
+      },
+      deleteUser:(state, action: PayloadAction< number>)=>{
+        const updateUsers = state.filter((user) => user.id !== action.payload);
+        localStorage.setItem("users", JSON.stringify(updateUsers));
+        return updateUsers;
+      },
+    },
+  }
+);
+
+export const {addUser, updateUser,deleteUser} = userSlice.actions;
+export default userSlice.reducer;
