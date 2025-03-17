e736a6c8dded8383fad84e049aad56f0bf9fe4c5
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-17 07:50:54 -0700

    update prettier

--- src/store/feature/userSlice.ts
+++ src/store/feature/userSlice.ts
@@ -2,44 +2,49 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { stringify } from "postcss";
 import { v4 as uuidv4 } from "uuid";
 
-interface User{
+interface User {
   id: string;
   name: string;
   email: string;
   isActive: boolean;
 }
 
-const initialState : User[] = JSON.parse(localStorage.getItem("users") || "[]");
+const initialState: User[] = JSON.parse(localStorage.getItem("users") || "[]");
 
 const userSlice = createSlice({
   name: "users",
   initialState,
-  reducers:{
-    addUser:(state, action:PayloadAction<{name: string; email: string}>)=>{
-      const newUser : User = {
+  reducers: {
+    addUser: (
+      state,
+      action: PayloadAction<{ name: string; email: string }>,
+    ) => {
+      const newUser: User = {
         id: uuidv4(),
         name: action.payload.name,
         email: action.payload.email,
         isActive: true,
-      }
+      };
       state.push(newUser);
-      localStorage.setItem("users", JSON.stringify(state))
+      localStorage.setItem("users", JSON.stringify(state));
     },
-    updateUser:(state, action: PayloadAction<{id: string; name: string; email: string}>)=>{
-      const user = state.find((user)=> user.id === action.payload.id);
-      if(user){
+    updateUser: (
+      state,
+      action: PayloadAction<{ id: string; name: string; email: string }>,
+    ) => {
+      const user = state.find((user) => user.id === action.payload.id);
+      if (user) {
         user.name = action.payload.name;
         user.email = action.payload.email;
         localStorage.setItem("users", JSON.stringify(state));
       }
     },
-    deleteUser:(state, action:PayloadAction<string>)=>{
-      const updateUsers = state.filter((user)=> user.id !== action.payload);
+    deleteUser: (state, action: PayloadAction<string>) => {
+      const updateUsers = state.filter((user) => user.id !== action.payload);
       localStorage.setItem("users", JSON.stringify(updateUsers));
       return updateUsers;
-
     },
-  }
-})
-export const {addUser, updateUser, deleteUser} = userSlice.actions;
+  },
+});
+export const { addUser, updateUser, deleteUser } = userSlice.actions;
 export default userSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  useReducer  from "./feature/userSlice";
+import useReducer from "./feature/userSlice";
 
 export const store = configureStore({
-  reducer:{
+  reducer: {
     users: useReducer,
-  }
-})
+  },
+});
 
-export type RooteStore = ReturnType <typeof store.getState>
+export type RooteStore = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
