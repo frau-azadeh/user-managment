8ab26cc5ed5ee3361ce86be981ac78ad8acfb90c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-17 05:40:06 -0700

    update

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import  useReducer  from "./feature/userSlice";
 
 export const store = configureStore({
   reducer:{
-
+    users: useReducer,
   }
 })
 
-export type RootState = ReturnType <typeof store.getState>
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
