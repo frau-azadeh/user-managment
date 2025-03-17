b5701c57800f4c00d56ad31a2d3bf49f00128c52
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-17 07:50:12 -0700

    refactor

--- src/store/feature/userSlice.ts
+++ src/store/feature/userSlice.ts
@@ -1,43 +1,45 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { stringify } from "postcss";
+import { v4 as uuidv4 } from "uuid";
 
 interface User{
-  id: number;
+  id: string;
   name: string;
   email: string;
   isActive: boolean;
 }
 
-const initialState : User[] = []
+const initialState : User[] = JSON.parse(localStorage.getItem("users") || "[]");
 
 const userSlice = createSlice({
   name: "users",
   initialState,
   reducers:{
-    addUser:(state, action: PayloadAction<{name: string; email: string}>)=>{
-      const newUser: User = {
-        id: Date.now(),
+    addUser:(state, action:PayloadAction<{name: string; email: string}>)=>{
+      const newUser : User = {
+        id: uuidv4(),
         name: action.payload.name,
         email: action.payload.email,
-        isActive:true,
+        isActive: true,
       }
       state.push(newUser);
       localStorage.setItem("users", JSON.stringify(state))
     },
-    updateUser:(state,action:PayloadAction<{id: number; name: string; email: string;}>)=>{
+    updateUser:(state, action: PayloadAction<{id: string; name: string; email: string}>)=>{
       const user = state.find((user)=> user.id === action.payload.id);
       if(user){
-        user.name= action.payload.name;
-        user.email= action.payload.email;
-        localStorage.setItem("users", JSON.stringify(state))
+        user.name = action.payload.name;
+        user.email = action.payload.email;
+        localStorage.setItem("users", JSON.stringify(state));
       }
     },
-    deleteUser:(state, action: PayloadAction<number>)=>{
-      const updateUsers = state.filter((user)=> user.id!== action.payload);
+    deleteUser:(state, action:PayloadAction<string>)=>{
+      const updateUsers = state.filter((user)=> user.id !== action.payload);
       localStorage.setItem("users", JSON.stringify(updateUsers));
-      return updateUsers
-    }
+      return updateUsers;
+
+    },
   }
 })
-
 export const {addUser, updateUser, deleteUser} = userSlice.actions;
-export default userSlice.reducer
+export default userSlice.reducer;
